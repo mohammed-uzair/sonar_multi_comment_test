dc00d4dbd37f31372b494217e19c189aaff5e555
mohammeduzair-oviva <mohammed.uzair@oviva.com>
2023-09-16 23:02:26 +0200

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,13 @@
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.sonarqube") version "4.3.1.3277"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "mohammed-uzair_sonar_multi_comment_test")
+        property("sonar.organization", "mohammed-uzair")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
