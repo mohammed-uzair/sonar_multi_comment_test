bcd333c5823fb78708555ef77962d5e9ea2c41ab
mohammeduzair-oviva <mohammed.uzair@oviva.com>
2023-09-17 00:08:28 +0200

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,6 @@ sonar {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
